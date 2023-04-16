42693a65067266ff12abf782d5e1dfcd42c6b465
Max Mynter <maxmynter@me.com>
2023-04-16 14:03:16 +0200

    Change Header Color to uiBlack

--- src/Styles/headerText.js
+++ src/Styles/headerText.js
@@ -1,3 +1,9 @@
-const headerTextStyles = { fontWeight: 900, fontSize: 32 };
+import { theme } from "./theme";
+
+const headerTextStyles = {
+  fontWeight: 900,
+  fontSize: 32,
+  color: theme.colors.uiBlack,
+};
 
 export default headerTextStyles;
